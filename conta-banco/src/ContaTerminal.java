816a21ae9c6c41490a867007af3380449a648768
Mizofabio <yokomizo.fabio@gmail.com>
2024-06-20 23:14:03 -0300

    Código pronto para entrega.

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
-public class ContaTerminal {
+
+ import java.util.Scanner;
+
+ public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("  Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favro, digite o número da Agência !");
+        Integer agencia = scanner.nextInt();
+
+        System.out.println("Insira o número da Conta !");
+        String numero = scanner.next();
+
+        System.out.println("Digite o seu Nome !");
+        String nome = scanner.next();
+
+        System.out.println("Digite o seu saldo!");
+        Double saldo = scanner.nextDouble();
+
+
+
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é "
+        + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        scanner.close();
+
     }
+
 }
